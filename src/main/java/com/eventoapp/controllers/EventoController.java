068744147609eee6ff8a7a58da717b2257d9dad6
Vinícius Yoda <vyoda4604@gmail.com>
2024-12-01 10:35:52 -0300

    materialize

--- src/main/java/com/eventoapp/controllers/EventoController.java
+++ src/main/java/com/eventoapp/controllers/EventoController.java
@@ -3,6 +3,7 @@ package com.eventoapp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.eventoapp.models.Evento;
 import com.eventoapp.repository.EventoRepository;
@@ -31,6 +32,14 @@ public class EventoController {
     // }
     // er.save(evento);
     // return "redirect:/cadastrarEvento";
-}
+// }
+
+    @RequestMapping("/eventos")
+    public ModelAndView listaEventos(){
+        ModelAndView mv = new ModelAndView("index");
+        Iterable<Evento> eventos = er.findAll();
+        mv.addObject("eventos", eventos);
+        return mv;
+    }
 
 }
